e5e84a92ecd14e65170cc663b5c3181bd8db7e6b
Egor Pristavka <50916604+mrglaster@users.noreply.github.com>
2022-10-10 11:16:43 +0800

    Update field_consts.cpp

--- four-in-a-row (четыре в ряд)/field_consts.cpp
+++ four-in-a-row (четыре в ряд)/field_consts.cpp
@@ -6,6 +6,6 @@ const char* CHAR_EMPTY = "- ";
 const char* RED_WIN_MESSAGE = "RED WON! CONGRATULATIONS!";
 const char* YELLOW_WIN_MESSAGE = "YELLOW WON! CONGRATULATIONS!";
 const char* TURN_SETTER = "Now turn is of: ";
-const char* GUIDE_TEXT = "Input the row in u'll add a color";
+const char* GUIDE_TEXT = "Input the number of the row in which u'll add the color";
 const char* CL_RED = "Red";
 const char* CL_YELLOW = "Yellow";
